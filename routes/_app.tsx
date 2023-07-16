7b83f020f2b8d87e0a5a8c48bd58e72ab5c34a33
Ryan Frantz <ryanleefrantz@gmail.com>
2023-07-16 15:25:14 -0400

    Adds htmx to play around with.

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,4 +1,5 @@
-import { Head } from "$fresh/runtime.ts";
+// asset() will automate adding cache headers to statiic assets.
+import { Head, asset } from "$fresh/runtime.ts";
 import { AppProps } from "$fresh/src/server/types.ts";
 import Header from "../components/Header.tsx";
 
@@ -7,6 +8,7 @@ export default function App({ Component }: AppProps) {
     <>
       <Head>
         <meta name="author" content="frantz" />
+        <script src={asset("/js/htmx@1.9.3.min.js")}></script>
       </Head>
       {/*
       <div class="dark:bg-gray-900 text-lg">

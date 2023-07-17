5495bf27de5b6ee147af03c421970abf9486fc47
Ryan Frantz <ryanleefrantz@gmail.com>
2023-07-17 13:16:24 -0400

    Move role save button to a component.

---
+++ components/SaveRoleButton.tsx
@@ -0,0 +1,12 @@
+export default function SaveRoleButton() {
+  return (
+    <div class="flex justify-end m-2">
+      <button
+        type="submit"
+        class="px-2 rounded bg-sky-500 hover:bg-sky-600 text-white text-sm"
+      >
+      Save Role
+      </button>
+    </div>
+  );
+}
--- routes/role/new.tsx
+++ routes/role/new.tsx
@@ -1,5 +1,6 @@
 import { Handlers } from "$fresh/server.ts";
 import RoleDetails from "../../components/RoleDetails.tsx";
+import SaveRoleButton from "../../components/SaveRoleButton.tsx";
 
 export const handler: Handlers = {
   async GET(_req, ctx) {
@@ -23,14 +24,7 @@ export const handler: Handlers = {
 export default function NewRole() {
   return (
     <form method="post" class="mt-1">
-	    <div class="flex justify-end m-2">
-	      <button
-          type="submit"
-          class="px-2 rounded bg-sky-500 hover:bg-sky-600 text-white text-sm"
-        >
-	      Save Role
-	      </button>
-	    </div>
+      <SaveRoleButton />
       <RoleDetails />
     </form>
   );

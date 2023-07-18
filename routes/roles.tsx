0f97ac9d9bc3311f9903eb3f29db7db6456122d7
Ryan Frantz <ryanleefrantz@gmail.com>
2023-07-18 10:50:08 -0400

    I settled on a table instead of a list.

--- routes/roles.tsx
+++ routes/roles.tsx
@@ -1,8 +1,7 @@
 import { Handlers, PageProps } from "$fresh/server.ts";
 // NOTE to self: Don't name components the same as routes; it creates an
 // infinite processing/rendering loop that Deno eventually bails from.
-// Hence RoleList rather than Roles below.
-//import RoleList from "../components/RoleList.tsx";
+// Hence RoleTable rather than Roles below.
 import RoleTable from "../components/RoleTable.tsx";
 import { getRoles } from "../lib/store.ts";
 

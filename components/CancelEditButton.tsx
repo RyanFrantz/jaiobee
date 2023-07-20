d11ab16b6f9aae4a4a98ba727a6c73da1e660ec7
Ryan Frantz <ryanleefrantz@gmail.com>
2023-07-20 07:29:04 -0400

    Adds new cancel button for role edits.

---
+++ components/CancelEditButton.tsx
@@ -0,0 +1,15 @@
+// FIXME: Think through how to have this call a resource via htmx and replace a
+// container. I'll need to revisit the /role/[id].tsx page to accommodate that.
+export default function CancelRoleButton({roleId}) {
+  return (
+    <div class="flex justify-end m-2">
+      <a href={"/role/" + roleId}>
+      <button
+        class="px-2 rounded bg-red-500 hover:bg-red-600 text-white text-sm"
+      >
+      Cancel
+      </button>
+      </a>
+    </div>
+  );
+}

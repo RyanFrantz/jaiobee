520b90728cb629c9d7beb0d896cbcc94d18e6166
Ryan Frantz <ryanleefrantz@gmail.com>
2023-07-25 19:10:15 -0400

    Removes unused code.

--- routes/role/[id]/note/add.tsx
+++ routes/role/[id]/note/add.tsx
@@ -1,12 +1,7 @@
 import { Handlers } from "$fresh/server.ts";
-import { render } from "https://esm.sh/preact-render-to-string@6.2.0";
-import NewNoteContainer from "../../../../components/NewNoteContainer.tsx";
 import { addNote } from "../../../../lib/store.ts";
 import { epoch } from "../../../../lib/utils.ts";
 
-// TODO: Add POST support that saves form input and them prepends the text
-// to the rows of notes.
-// Returns hypermedia that will replace a container from whence it was called.
 export const handler: Handlers = {
   async POST(req) {
     const url = new URL(req.url);
@@ -24,6 +19,7 @@ export const handler: Handlers = {
         "created-at": epoch(),
         message: inputs["new-note"]
       }
+    // FIXME: Test for failure.
     await addNote(userId, roleId, note)
     const headers = new Headers();
     // This should trigger an event on the page that called this endpoint which
@@ -33,19 +29,5 @@ export const handler: Handlers = {
       status: 200,
       headers,
     });
-  },
-  async GET() {
-    const fragment = render(
-      <NewNoteContainer />
-    );
-    const headers = new Headers();
-    headers.set("content-type", "text/html");
-    return new Response(
-      fragment,
-      {
-        headers: headers
-      }
-    );
-    
   },
 };

19490e4f856b1f8527ae9d8e534b69899d12f365
Ryan Frantz <ryanleefrantz@gmail.com>
2023-07-31 19:03:19 -0400

    Do not persist storage for supabase client.
    - This prevents the following warning from being displayed on startup:
    > No storage option exists to persist the session, which may result in unexpected behavior when using auth.
    > If you want to set persistSession to true, please provide a storage option or you may set persistSession to false to disable this warning.

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -3,6 +3,8 @@ import { createClient } from "https://esm.sh/@supabase/supabase-js@2.31.0";
 const SUPABASE_PROJECT_URL = Deno.env.get("SUPABASE_PROJECT_URL") || "please-define-url";
 const SUPABASE_ANON_KEY = Deno.env.get("SUPABASE_ANON_KEY") || "please-define-key";
 
-const supabase = createClient(SUPABASE_PROJECT_URL, SUPABASE_ANON_KEY);
+const supabase = createClient(SUPABASE_PROJECT_URL, SUPABASE_ANON_KEY, {
+  auth: { persistSession: false }
+});
 
 export default supabase;

a3d526579c7c74c8f78f5ea876dd423744afa083
Ryan Frantz <ryanleefrantz@gmail.com>
2023-08-21 11:02:20 -0400

    Check for authentication on login and signup pages.

--- routes/login.tsx
+++ routes/login.tsx
@@ -1,18 +1,19 @@
 import AuthenticationForm from "../components/AuthenticationForm.tsx";
 import { Handlers } from "$fresh/server.ts";
+import { isAuthenticated } from "../lib/utils.ts";
 
 export const handler: Handlers = {
   async GET(_req, ctx) {
-    let activeSession = false;
-    return ctx.render({ activeSession: activeSession });
+    const isAuthned = isAuthenticated(ctx);
+    return ctx.render({ isAuthenticated: isAuthned });
   },
 };
 
 export default function Login(props) {
-  const { activeSession } = props.data;
+  const { isAuthenticated } = props.data;
   return (
     <>
-      {activeSession ? <h3>You're already logged in!</h3> : (
+      {isAuthenticated ? <h3>You're already logged in!</h3> : (
         <div>
           <AuthenticationForm />
         </div>
--- routes/signup.tsx
+++ routes/signup.tsx
@@ -1,23 +1,24 @@
 import AuthenticationForm from "../components/AuthenticationForm.tsx";
 import { Handlers } from "$fresh/server.ts";
+import { isAuthenticated } from "../lib/utils.ts";
 
 export const handler: Handlers = {
   async GET(req, ctx) {
-    let activeSession = false;
+    const isAuthned = isAuthenticated(ctx);
     const url = new URL(req.url);
     const error = url.searchParams.get("error");
     if (error) {
       return ctx.render({ error: error });
     }
-    return ctx.render({ activeSession: activeSession });
+    return ctx.render({ isAuthenticated: isAuthned });
   },
 };
 
 export default function Login(props) {
-  const { activeSession, error } = props.data;
+  const { isAuthenticated, error } = props.data;
   return (
     <>
-      {activeSession ? <h3>You're already logged in!</h3> : (
+      {isAuthenticated ? <h3>You're already logged in!</h3> : (
         <div>
           {error &&
             (

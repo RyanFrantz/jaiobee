6fc8ca806201491937e8d2307e21d647a78d8c0c
Ryan Frantz <ryanleefrantz@gmail.com>
2023-08-21 12:03:29 -0400

    Add free trial call-to-action.

---
+++ components/FreeTrialCTA.tsx
@@ -0,0 +1,12 @@
+export default function FreeTrialCTA() {
+  return (
+    <div class="flex justify-center">
+      <a
+        href="/signup"
+        class="mt-4 p-1 w-1/2 text-white text-center bg-emerald-500 rounded-full"
+      >
+        Sign up for a free week!
+      </a>
+    </div>
+  );
+}
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,9 +1,25 @@
 import FeatureAllRoles from "../components/FeatureAllRoles.tsx";
 import FeatureRoleDetails from "../components/FeatureRoleDetails.tsx";
+import FreeTrialCTA from "../components/FreeTrialCTA.tsx";
+import { Handlers } from "$fresh/server.ts";
+import { isAuthenticated } from "../lib/utils.ts";
 
-export default function Home() {
+export const handler: Handlers = {
+  async GET(_req, ctx) {
+    const isAuthned = isAuthenticated(ctx);
+    return ctx.render({ isAuthenticated: isAuthned });
+  },
+};
+
+export default function Home(props) {
+  const { isAuthenticated, error } = props.data;
   return (
     <>
+    { isAuthenticated ? (
+      <></>
+    ) : (
+      <FreeTrialCTA />
+    )}
       <FeatureAllRoles />
       <FeatureRoleDetails />
     </>

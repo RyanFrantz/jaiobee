40891dbaafafae2c602b77735f71955f38418d07
Ryan Frantz <ryanleefrantz@gmail.com>
2023-08-21 22:20:32 -0400

    Prep for static Tailwind stylesheet.

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./routes/**/*.{tsx,ts}",
+    "./islands/**/*.{tsx,ts}",
+    "./components/**/*.{tsx,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+

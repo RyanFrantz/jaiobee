a4173955033502761b984f11a502a1e0616d12c6
Ryan Frantz <ryanleefrantz@gmail.com>
2023-08-21 20:46:13 -0400

    Use better fake URL strings.

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,9 +1,9 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2.31.0";
 
 const SUPABASE_PROJECT_URL = Deno.env.get("SUPABASE_PROJECT_URL") ||
-  "please-define-url";
+  "https://please.define.url.com";
 const SUPABASE_ANON_KEY = Deno.env.get("SUPABASE_ANON_KEY") ||
-  "please-define-key";
+  "https://please.define.key.com";
 
 const supabase = createClient(SUPABASE_PROJECT_URL, SUPABASE_ANON_KEY, {
   auth: { persistSession: false },

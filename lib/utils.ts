9edc3e0f07811b0d6b5e9bfa090d1741d1d7c69a
Ryan Frantz <ryanleefrantz@gmail.com>
2023-08-28 04:07:26 -1000

    #5 Prep timezone support.

--- lib/utils.ts
+++ lib/utils.ts
@@ -7,24 +7,17 @@ const epoch = () => {
   return Date.now();
 };
 
-/*
-> e = new Date(1689627698000)
-2023-07-17T21:01:38.000Z
-> e.toLocaleString()
-'7/17/2023, 5:01:38 PM'
-> e.toLocaleString('en-GB', {timeZone: 'UTC'})
-'17/07/2023, 21:01:38'
-> e.toLocaleString('en-US', {timeZone: 'UTC'})
-'7/17/2023, 9:01:38 PM'
-> e.toLocaleString('en-US', {timeZone: 'US/Eastern'})
-'7/17/2023, 5:01:38 PM'
-> e.toLocaleString('en-US', {timeZone: 'US/Pacific'})
-'7/17/2023, 2:01:38 PM'
-*/
-const epochToLocale = (epoch: string): string => {
+const epochToLocale = (
+  epoch: string,
+  locale: string = "en-US",
+  timeZone: string = "America/New_York",
+): string => {
   const millis = parseInt(epoch);
   const d = new Date(millis);
-  return d.toLocaleString(); // TODO: You know! Timezones!
+  return d.toLocaleString(locale, {
+    timeZone: timeZone,
+    timeZoneName: "short", // EDT, PDT, etc.
+  });
 };
 
 /* A set of regular expressions representing routes we'll use to check for

2f1295e116b01e3fb7281d6f3364f63ddeef5eb4
Ryan Frantz <ryanleefrantz@gmail.com>
2023-08-31 12:58:05 -0400

    Set a cookie containing locale and timezone.
    - We're going hella simple here.
    - Undoes all the context-related work I had attempted earlier.
      - Instead, our middleware can pick up the `locale` cookie and use it
        in request context.
    - Related to #5.

--- islands/DetectUserLocale.tsx
+++ islands/DetectUserLocale.tsx
@@ -1,15 +1,25 @@
 import { useEffect } from "preact/hooks";
 
-// Detect the user's locale and date formatting options and set the
-// resulting object as a signal value we can use in other components.
-export default function DetectUserLocale({ locale, timeZone }) {
+// Detect the user's locale and date formatting options and set a cookie
+// with that information. This island is expected to be run for every request
+// so we will be able to detect if these values change.
+export default function DetectUserLocale() {
   useEffect(() => {
     // Alias the destructured properties to avoid collisons.
-    const { locale: browserLocale, timeZone: browserTimeZone } = Intl
-      .DateTimeFormat().resolvedOptions();
-    // Update the relevant signals.
-    locale.value = browserLocale;
-    timeZone.value = browserTimeZone;
+    const { locale , timeZone } = Intl.DateTimeFormat().resolvedOptions();
+    const cookieValue = btoa(JSON.stringify({locale: locale, timeZone: timeZone}));
+    // In local dev, the host can include a port number and cookies won't get set.
+    // Use hostname for consistency.
+    const domain = new URL(location.origin).hostname;
+    const localeCookie = [
+      `locale=${cookieValue}`,
+      `Domain=${domain}`,
+      "Path=/",
+      "Max-Age=3600",
+      "SameSite=lax",
+      "Secure"
+    ].join("; ");
+    document.cookie = localeCookie; // Set the cookie.
   }, []);
 
   // Nothing to see here. Writing this return for completeness.

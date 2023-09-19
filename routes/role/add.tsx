f48d19363e9321f64c1d6272adf8fb0f9cbb0c56
Ryan Frantz <ryanleefrantz@gmail.com>
2023-09-18 20:04:53 -0400

    Add contact to role adds.
    - Missed this for #8.

--- routes/role/add.tsx
+++ routes/role/add.tsx
@@ -1,12 +1,17 @@
-import { Handlers } from "$fresh/server.ts";
+import { Handlers, PageProps } from "$fresh/server.ts";
 import SaveRoleButton from "../../components/SaveRoleButton.tsx";
 import RoleForm from "../../components/RoleForm.tsx";
 import { addRole } from "../../lib/store.ts";
 import { epoch } from "../../lib/utils.ts";
+import { getContacts } from "../../lib/store.ts";
 
 export const handler: Handlers = {
   async GET(_req, ctx) {
-    return await ctx.render();
+    const userId = ctx.state.userId;
+    // IDEA: Since userId is available in a cookie, use htmx and an API route
+    // to return the contacts as a set of select options.
+    const contacts = await getContacts(userId);
+    return await ctx.render({ contacts });
   },
   async POST(req, ctx) {
     const formData = await req.formData();
@@ -34,7 +39,8 @@ export const handler: Handlers = {
   },
 };
 
-export default function AddRole() {
+export default function AddRole(props: PageProps) {
+  const { contacts } = props.data;
   // The user is adding the role for the first time so we'll pre-populate the
   // important bits.
   const now = epoch();
@@ -42,7 +48,7 @@ export default function AddRole() {
   return (
     <div id="role-container">
       <SaveRoleButton />
-      <RoleForm role={role} action="add" />
+      <RoleForm role={role} contacts={contacts} action="add" />
     </div>
   );
 }
